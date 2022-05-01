9ae1a62bb595ef51971d890518ce8637d5c1b55d
Md Ishaque <iamishaque.business@gmail.com>
2022-05-01 20:59:22 +0600

    set up faq api

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-import 'dotenv/config'
-import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
-import itemRoutes from './routes/Items.js';
+import 'dotenv/config';
+import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors())
+// app.use(express.json())
 app.use(express.json({limit: "30mb",extended:true}));
 app.use(express.urlencoded({limit: "30mb",extended:true}));
-
-app.use('/items',itemRoutes)
 
 
 app.get('/',(req,res)=>{
@@ -24,35 +22,66 @@ app.get('/',(req,res)=>{
 // connect with mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rgm8.mongodb.net/greenWarehouseDatabase?retryWrites=true&w=majority`;
-mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=>app.listen(port,console.log(`Server running on ${port}`))).catch(err=>console.log(err));
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function runServer(){
+    try {
+        // database connection
+        await client.connect();
+        const productCollection = client.db("WarehouseManagement").collection("products");
+        const feedbackCollection = client.db("WarehouseManagement").collection("feedbacks");
+        const faqCollection = client.db("WarehouseManagement").collection("faq");
 
+        // Product Api
 
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        // get all the data
+        app.get('/items',async (req,res)=>{
+            const cursor = productCollection.find({})
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+        // insert and update
+        app.post('/items',async (req,res)=>{
+            const item = req.body;
+            item._id && delete item._id;
+            // create filter for an old product to update
+            const filter = {productId:item.productId} ;
+            // create a new item if no item is matched
+            const options = {upsert:true};
+            // update a item
+            const updateItem = {
+                $set:{...item}
+            }
+            const result = await productCollection.updateOne(filter,updateItem,options)
+            res.send(result)
+        })
 
-// async function runServer(){
-//     try {
-//         // database connection
-//         await client.connect();
-//         const productCollection = client.db("GreenHouse").collection("products");
+         // Feedback Api
 
-//         // Product Api
+        // get all the data
+        app.get('/feedbacks',async (req,res)=>{
+            const cursor = feedbackCollection.find({})
+            const items = await cursor.toArray();
+            res.send(items)
+        })
 
-//         // get all the data
-//         app.get('/item',async (req,res)=>{
-//             const cursor = productCollection.find({})
-//             const items = await cursor.toArray();
-//             res.send(items)
-//         })
+         // FAQ Api
+
+        // get all the data
+        app.get('/faq',async (req,res)=>{
+            const cursor = feedbackCollection.find({})
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+
 
        
         
-//     } finally {
+    } finally {
         
-//     }
-// }
-// runServer().catch(console.dir);
-// server listening
+    }
+}
+runServer().catch(console.dir);
 
 
+app.listen(port,console.log(`Server running on ${port}`))
