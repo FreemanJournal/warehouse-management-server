fad52d727c762158bb4bbac4e8846bd9052155a4
Md Ishaque <iamishaque.business@gmail.com>
2022-05-05 00:12:43 +0600

    some minor changes

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
+import jsonwebtoken from 'jsonwebtoken';
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
-
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors())
-// app.use(express.json())
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
@@ -24,6 +21,24 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3rgm8.mongodb.net/greenWarehouseDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+// verify jwt token
+function verifyAuthToken(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized Access." })
+    }
+    const accessToken = authHeader.split(" ")[1]
+    jsonwebtoken.verify(accessToken, process.env.ACCESS_TOKEN_PRIVATE_KEY, function (error, decoded) {
+        if (error) {
+            return res.status(403).send({ message: "Forbidden Access" })
+        }
+        req.decoded = decoded
+        next();
+    })
+
+}
+
 async function runServer() {
     try {
         // database connection
@@ -34,13 +49,15 @@ async function runServer() {
 
         // Product Api
 
-        // get all the data
+        // get all the products
         app.get('/items', async (req, res) => {
             const cursor = productCollection.find({})
             const items = await cursor.toArray();
             res.send(items)
         })
-        // insert and update
+
+
+        // insert and update products
         app.put('/items', async (req, res) => {
             const item = req.body;
             // item._id && delete item._id;
@@ -72,6 +89,7 @@ async function runServer() {
             res.send(result)
         })
 
+
         // Feedback Api
 
         // get all the data
@@ -86,6 +104,27 @@ async function runServer() {
         // get all the data
         app.get('/faq', async (req, res) => {
             const cursor = faqCollection.find({})
+            const items = await cursor.toArray();
+            res.send(items)
+        })
+
+        // create token
+        app.post("/createToken", async (req, res) => {
+            const email = req.body
+            const authAccessToken = jsonwebtoken.sign(email, process.env.ACCESS_TOKEN_PRIVATE_KEY, { expiresIn: "1d" })
+            res.send({ authAccessToken })
+        })
+
+        // get logged in user products
+        app.get('/items-user', verifyAuthToken, async (req, res) => {
+            const { email } = req.query
+            const decodedEmail = req.decoded.email
+            console.log(decodedEmail, email);
+            if (email !== decodedEmail) {
+                res.status(403).send({ message: "Forbidden Access" })
+                return;
+            }
+            const cursor = productCollection.find({ userEmail: email })
             const items = await cursor.toArray();
             res.send(items)
         })
